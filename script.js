dc3e21a8c246bcbf6c969b7e256bee6dc264878f
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-01-11 15:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+ for(let i=0;i<str.length-1;i++){
+	 let count=0;
+	 if(str[i]==str[i+1]){
+		 count++;
+	 }else{
+		 if(count==0){
+			 return str[i];
+		 }
+	 }
+ }
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
